96b95d6b7613af2a5386e2727f6d0f98135b3b77
s61494 <s61494@beuth-hochschule.de>
2017-09-27 22:15:06 +0200

    add all

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
+  database: 'mongodb://root2:root2@ds151941.mlab.com:51941/mongo-bongo',    //dev
+  //database: 'mongodb://mongodb://localhost:27017/ng-movies',
+  secret: 'yoursecret'
+}
